da723e379d6d1a0370eb06cef5ecbd1864de6dfb
Siddharth Joshi <sjoshi804@gmail.com>
2018-10-24 14:34:54 -0700

    Update Faulty-Keyboard:Solution.cpp

--- Faulty-Keyboard:Solution.cpp
+++ Faulty-Keyboard:Solution.cpp
@@ -1,1 +1,30 @@
+//Input and output was handled by Hacker Rank
+//Testees had to only complete a single function that was passed in the string typed in and would return the output on the faulty keyboard
+#incude <cctype.h> //Extra include statement required for islower, toupper etc. functions
 
+string faultyKeyboard(string input)
+{
+  bool CapsLockOn = false;
+  
+  for (int i  = 0; i < input.size();)
+  {
+    if (input[i] == 'a' || input[i] == 'A')
+    {
+      CapsLockOn = ! CapsLockOn;
+      input.erase(i, 1);
+      continue;
+    }
+    
+    if (CapsLockOn)
+    {   
+      if (islower(input[i]))
+        input[i] = toupper(input[i]);
+      else
+        input[i] = tolower(input[i]);
+    }
+    
+    i++;
+  }
+  
+  return input;
+}
